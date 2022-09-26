475261f80677792c7ac3dba9ed36274c551dbc21
Adrian Lorenčič <adrianlorencic98@gmail.com>
2022-09-26 22:31:48 +0200

    Camera movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public FixedJoystick joystick;
+    public Animator animator;
+
+    public float moveSpeed = 5;
+   
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(joystick.Horizontal * moveSpeed, rb.velocity.y, joystick.Vertical * moveSpeed);
+
+        if(joystick.Horizontal != 0 && joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        }
+    }
+}
