9d56f0493fa49f7879111f0c325ae0a6e17f2a0c
Adrian Lorenčič <adrianlorencic98@gmail.com>
2022-10-10 18:41:03 +0200

    Character Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,86 +14,48 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float jumpHeight = 1.0f;
+    private float jumpHeight = 50.0f;
     private float gravityValue = -9.81f;
+    float ySpeed;
 
     private void Start()
     {
     }
 
-    void Update()
+    private void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        Vector3 movementDirection = new Vector3(horizontal, 0 , vertical);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * playerSpeed;
+        movementDirection.Normalize();
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerVelocity.y = 0f;
+            ySpeed = jumpForce;
         }
 
-        Vector3 move = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
 
-        if (move != Vector3.zero)
+        controller.Move(velocity * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero)
         {
-            gameObject.transform.forward = move;
+            gameObject.transform.forward = movementDirection;
             animator.SetBool("Run", true);
         }
         else
         {
             animator.SetBool("Run", false);
         }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
     }
-
     public void Jump()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, 1 + 0.1f))
-        {
-            playerVelocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
-            animator.SetTrigger("Jump");
-        }
+        ySpeed = jumpForce;
+        animator.SetTrigger("Jump");
     }
-    /*
-    public Rigidbody rb;
-    public FixedJoystick joystick;
-    public Animator animator;
-    public CharacterController characterController;
-    public Transform groundCheck;
-    public LayerMask groundMask;
-
-    public float moveSpeed = 5, gravity = -9.81f, groundDistance = 0.2f, turnSmoothTime = 0.00001f;
-    public int jumpForce = 5;
-
-    Vector3 velocity;
-    float horizontal, vertical, turnSmoothVelocity;
-    public bool isGrounded;
-
-    private void Update()
-    {
-        JoystickMovementAndMoveAnimations();
-    }
-
-    private void JoystickMovementAndMoveAnimations()
-    {
-        horizontal = -joystick.Horizontal;
-        vertical = -joystick.Vertical;
-
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
-     
-        characterController.Move(move * moveSpeed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-
-        characterController.Move(velocity * Time.deltaTime);
-    }
-
-    public void Jump()
-    {
-        if(Physics.Raycast(transform.position, Vector3.down, 1 + 0.1f))
-        {
-            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
-            animator.SetTrigger("Jump");
-        }
-    }*/
 }
